cabed6b7226c643ee6345e15b5b828ff9c1c4be8
shivraj <shivraj@micronxt.com>
2024-06-06 19:03:39 +0530

    last commit

---
+++ src/pages/profile.tsx
@@ -0,0 +1,14 @@
+import React, { useEffect } from "react";
+
+const Profile = () => {
+useEffect(() => {
+console.log("Test net")
+}, [])
+  return (
+    <div className="">
+      Shivraj Patil
+    </div>
+  );
+};
+
+export default Profile;
